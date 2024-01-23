4a238c064ec5991f14712333999318b1d81660f1
Chew <101428141+c4hun@users.noreply.github.com>
2024-01-23 14:44:07 +0100

    Create Hello_YOU.sh
    :3

---
+++ Hello_YOU.sh
@@ -0,0 +1,9 @@
+echo "What is your name?"
+
+read name
+
+echo "Hello, $name!!"
+sleep 1
+echo "How are you, $name, you look great :)"
+sleep 1
+echo "Hope you have the best day ever today, $name. See you soon!!"
